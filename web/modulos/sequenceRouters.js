b04e2e4213556ddce33c61351010f457e73f26ec
Fabio Lenine <fabiolenine@gmail.com>
2016-09-07 07:45:12 -0300

    Parte 03
    Alteração da extensão dos arquivos de html para ejs. Implementação das
    rotas de todos os arquivos ejs. Alteração do caminho da rota do
    angularjs.

--- web/modulos/sequenceRouters.js
+++ web/modulos/sequenceRouters.js
@@ -1,30 +1,30 @@
 angular.module("sequenceRouter",[]).config(function ($routeProvider){
 	$routeProvider.when("/",{
-		templateUrl:"./views/contentSequenceDashboard.html",
+		templateUrl:"/contentSequenceDashboard.html",
 		controller:"sequenceControllerMain"
 	})
 	.when("/dashboard/sequence",{
-		templateUrl:"./views/contentSequenceDashboard.html",
+		templateUrl:"/contentSequenceDashboard.html",
 		controller:"sequenceControllerMain"
 	})
 	.when("/sequence/layouts",{
-		templateUrl:"./views/contentSequenceLayouts.html",
+		templateUrl:"/contentSequenceLayouts.html",
 		controller:"sequenceControllerLayouts"
 	})
 	.when("/sequence/lista",{
-		templateUrl:"./views/contentSequenceLista.html",
+		templateUrl:"/contentSequenceLista.html",
 		controller:"sequenceControllerLista"
 	})
 	.when("/sequence/servicos",{
-		templateUrl:"./views/contentSequenceServicos.html",
+		templateUrl:"/contentSequenceServicos.html",
 		controller:"sequenceControllerService"
 	})
 	.when("/sequence/configuracao",{
-		templateUrl:"./views/contentSequenceConfiguracao.html",
+		templateUrl:"/contentSequenceConfiguracao.html",
 		controller:"sequenceControllerConfig"
 	})
 	.when("/404",{
-		templateUrl:"./views/404in.html",
+		templateUrl:"/404in.html",
 		controller:"sequenceControllerMain"
 	})
 	.otherwise({
--- ws/routers/routerSequence.js
+++ ws/routers/routerSequence.js
@@ -1,9 +1,67 @@
 // Rota dos sites Cartório Moreira de Deus
 module.exports = function(app, passport, dbservicos, dbescolhas, dbchamadas)
 {	
-	app.get('/',function(req, res) {
+	
+	// Web Page
+	
+	app.get('/', function(req, res) {
 		res.render('login.ejs', {message: req.flash('loginMessage')});
 	});
+	
+	app.get('/main(.html)?', isLoggedIn, function(req, res) {
+		res.render('main.ejs', {usuario: req.user});
+	});
+	
+	app.get('/signup', function(req, res) {
+		res.render('signup.ejs', {message: req.flash('signupMessage')});
+	});
+	
+	app.get('/perfil(.html)?', isLoggedIn, function(req, res) {
+		res.render('perfil.ejs', {usuario: req.user});
+	});
+	
+	app.get('/contentSequenceConfiguracao(.html)?', isLoggedIn, function(req, res) {
+		res.render('contentSequenceConfiguracao.ejs', {usuario: req.user});
+	});
+	
+	app.get('/contentSequenceDashboard(.html)?', isLoggedIn, function(req, res) {
+		res.render('contentSequenceDashboard.ejs', {usuario: req.user});
+	});
+	
+	app.get('/contentSequenceLayouts(.html)?', isLoggedIn, function(req, res) {
+		res.render('contentSequenceLayouts.ejs', {usuario: req.user});
+	});
+	
+	app.get('/contentSequenceLista(.html)?', isLoggedIn, function(req, res) {
+		res.render('contentSequenceLista.ejs', {usuario: req.user});
+	});
+	
+	app.get('/contentSequenceServicos(.html)?', isLoggedIn, function(req, res) {
+		res.render('contentSequenceServicos.ejs', {usuario: req.user});
+	});
+	
+	app.get('/contentSequenceServicosFULLSCR(.html)?', isLoggedIn, function(req, res) {
+		res.render('contentSequenceServicosFULLSCR.ejs', {usuario: req.user});
+	});
+	
+	app.get('/painelLayout01(.html)?', isLoggedIn, function(req, res) {
+		res.render('painelLayout01.ejs', {usuario: req.user});
+	});
+	
+	app.get('/painelLayout02(.html)?', isLoggedIn, function(req, res) {
+		res.render('painelLayout02.ejs', {usuario: req.user});
+	});
+	
+	app.get('/404in(.html)?', isLoggedIn, function(req, res) {
+		res.render('404in.ejs', {usuario: req.user});
+	});
+	
+	app.get('/sair', function(req,res) {
+		req.logout();
+		res.redirect('/');
+	});
+	
+	// API
 	
 	app.route('/api/sequence/v001/servicos')
 	.post(function(req, res){
@@ -54,16 +112,20 @@ module.exports = function(app, passport, dbservicos, dbescolhas, dbchamadas)
 		});
 	});
 	
-	const pathDir = '/Users/fabiolenine/Documents/Projetos/notarysoffice/web/views/';
+	// Geral
 	
 	// Tratamentos dos erros 404 e 500
 	app.use(function(req, res, next) {
-  		res.status(404).sendFile(pathDir + '404.html');
+  		res.status(404).render('404.ejs');
 	});
 
 	app.use(function(err, req, res, next) {
 	  	console.error(err.stack);
-		res.status(500).sendFile(pathDir + '500.html');
+		res.status(500).render('500.ejs');
 	});
 
+	function isLoggedIn(req, res, next) {
+		if (req.isAuthenticated()) return next();
+		res.redirect('/');
+	};
 }
